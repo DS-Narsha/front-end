3838802ce0d01484037b8985ddf478672907dafb
[ninano05] <[ohjy0530@duksung.ac.kr]>
2023-05-11 19:14:21 +0900

    [commit] 네비게이션 바 주석 [commit] 네비게이션 바 주석

--- narsha_front/App.tsx
+++ narsha_front/App.tsx
@@ -3,6 +3,6 @@ import MainNavigator from './src/screens/MainNavigator';
 
 export default function App() {
   return (
-      <MainNavigator />
+      <MainNavigator /> //네비게이션 바 호출
     )
 }
--- narsha_front/declarations.d.ts
+++ narsha_front/declarations.d.ts
@@ -1,4 +1,5 @@
 declare module '*.svg' {
+  //SVG 파일을 인식하기 위한 코드
   import React from 'react';
   import {SvgProps} from 'react-native-svg';
   const content: React.FC<SvgProps>;
