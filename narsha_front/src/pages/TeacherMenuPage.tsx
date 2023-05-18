d0fddc1621acc2de1d8e47c87c832a1a64eab0e5
yxxyn20 <dbtndus0527@gmail.com>
2023-05-18 13:59:47 +0900

    [rename] 파일명, 폴더명 수정 및 파일 이동 - 파일명과 폴더명을 컨벤션에 맞추어 수정 - 파일을 적절한 폴더에 이동 - screen 폴더 삭제, stack 폴더 생성

---
+++ narsha_front/src/pages/TeacherMenuPage.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {Text, View, StyleSheet, Button} from 'react-native';
+import Notification from '../assets/teacherMenu/notification.svg';
+import StudentList from '../assets/teacherMenu/studentList.svg';
+import AppTime from '../assets/teacherMenu/appTime.svg';
+import GroupCode from '../assets/teacherMenu/groupCode.svg';
+import Undefinedfunc from '../assets/teacherMenu/undefinedSetting.svg';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+export default function TeacherMenu({navigation}: any) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.container2}>
+        <TouchableOpacity>
+          <View style={{padding: 15}}>
+            <Notification />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={{padding: 15}}>
+            <StudentList />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={{padding: 15}}>
+            <AppTime />
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container3}>
+        <TouchableOpacity>
+          <View style={{padding: 15}}>
+            <GroupCode />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={{padding: 15}}>
+            <Undefinedfunc />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={{padding: 15}}>
+            <Undefinedfunc />
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container2: {
+    height: 'auto',
+    width: 'auto',
+    flexDirection: 'row',
+  },
+  container3: {
+    height: 'auto',
+    width: 'auto',
+    flexDirection: 'row',
+  },
+});
