0f42e570e195c745215f0bfc957018b0e28e1970
yxxyn20 <dbtndus0527@gmail.com>
2023-06-05 06:46:24 +0900

    [feat] main stack에 적용: stack header에 아이콘 추가, 아이콘을 눌렀을 때 페이지 이동 구현 - 이전 커밋과 내용 동일 - headerRight, onPress 사용

--- narsha_front/src/components/navigation/MainStack.tsx
+++ narsha_front/src/components/navigation/MainStack.tsx
@@ -2,11 +2,11 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {View} from 'react-native';
 import BackSvg from '../../assets/back.svg';
-
 import Main from '../../pages/MainPage';
 // import AlarmPage from '../../pages/AlarmPage';
 import NoticeList from '../../pages/NoticeListPage';
 import NoticeWritePage from '../../pages/NoticeWritePage';
+import Write from '../../assets/write.svg';
 
 const Stack = createStackNavigator();
 export default function MainNavigatorStack() {
@@ -37,12 +37,25 @@ export default function MainNavigatorStack() {
         <Stack.Screen
           name="NoticeList"
           component={NoticeList}
-          options={{title: '공지 목록'}}
+          options={({navigation}) => ({
+            title: '공지 목록',
+            headerRight: () => {
+              return (
+                <View style={{marginRight: 16}}>
+                  <Write
+                    onPress={() => {
+                      navigation.navigate('NoticeWritePage');
+                    }}
+                  />
+                </View>
+              );
+            },
+          })}
         />
         <Stack.Screen
           name="NoticeWritePage"
           component={NoticeWritePage}
-          options={{title: '공지 작성'}}
+          options={{title: '공지 작성 페이지'}}
         />
         {/* <Stack.Screen name="AlarmPage" component={AlarmPage} /> */}
       </Stack.Navigator>
