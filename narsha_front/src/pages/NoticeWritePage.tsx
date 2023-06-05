488836b855e94d0fdbdf898810b9afae4cf59c78
yxxyn20 <dbtndus0527@gmail.com>
2023-06-05 21:16:41 +0900

    [style] 공지 작성 페이지 스타일 수정 - 업로드 버튼에 그림자 추가 - shadow와 elevation 적용

--- narsha_front/src/pages/NoticeWritePage.tsx
+++ narsha_front/src/pages/NoticeWritePage.tsx
@@ -6,57 +6,70 @@ import MultiTextInput from '../components/MultiTextInput';
 // 공지 작성 페이지
 
 export default function NoticeWritePage({navigation}: any) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.container2}>
-                <View style={styles.titlecontainer}><Text style={styles.title}>공지 제목</Text></View>
-                <View style={styles.textinput}><MyTextInput placeholder=""/></View>
-                <View style={styles.titlecontainer}><Text style={styles.title}>공지 내용</Text></View>
-                <View style={styles.textinput}><MultiTextInput placeholder=""/></View>
-            </View>
-            <TouchableOpacity onPress={() => navigation.navigate('InfoList')}>
-                <View style={styles.updatebtn}>
-                <Text style={styles.btntitle}>공지 올리기</Text>
-                </View>
-            </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+      <View style={styles.container2}>
+        <View style={styles.titlecontainer}>
+          <Text style={styles.title}>공지 제목</Text>
         </View>
-    );
-};
+        <View style={styles.textinput}>
+          <MyTextInput placeholder="" />
+        </View>
+        <View style={styles.titlecontainer}>
+          <Text style={styles.title}>공지 내용</Text>
+        </View>
+        <View style={styles.textinput}>
+          <MultiTextInput placeholder="" />
+        </View>
+      </View>
+      <TouchableOpacity onPress={() => navigation.navigate('InfoList')}>
+        <View style={styles.updatebtn}>
+          <Text style={styles.btntitle}>공지 올리기</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FCFDE1',
-        flexDirection: 'column',
-        alignItems: 'center',
+  container: {
+    flex: 1,
+    backgroundColor: '#FCFDE1',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  container2: {
+    marginTop: 55,
+  },
+  textinput: {
+    width: 350,
+    margin: 10,
+  },
+  title: {
+    fontWeight: '200',
+    fontSize: 15,
+    color: '#000000',
+  },
+  titlecontainer: {
+    marginHorizontal: 20,
+  },
+  updatebtn: {
+    backgroundColor: '#AADF98',
+    height: 51,
+    width: 195,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    marginTop: 40,
+    shadowOffset: {
+      width: 0,
+      height: 3,
     },
-    container2:{
-        marginTop: 55,
-    },
-    textinput:{
-        width: 350,
-        margin: 10,
-    },
-    title: {
-        fontWeight: '200',
-        fontSize: 15,
-        color: '#000000',
-    },
-    titlecontainer:{
-        marginHorizontal: 20,
-    },
-    updatebtn: {
-        backgroundColor: '#AADF98',
-        height: 51,
-        width: 195,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 30,
-        marginTop: 40,
-    },
-    btntitle:{
-        color: '#000000',
-        fontSize: 15,
-        fontWeight: '200',
-    }
-})
+    elevation: 5,
+  },
+  btntitle: {
+    color: '#000000',
+    fontSize: 15,
+    fontWeight: '200',
+  },
+});
