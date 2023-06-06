09e0666146f537224d0f77143b8413107afb5f58
yxxyn20 <dbtndus0527@gmail.com>
2023-06-06 18:46:00 +0900

    [rename] login 폴더를 signup 폴더의 하위 폴더로 이동 - login 폴더를 signup 폴더의 하위 폴더로 이동

---
+++ narsha_front/src/pages/SignUp/login/LoginPage.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import MyTextInput from '../../../components/MyTextInput';
+import AppLogo from '../../assets/app-logo.svg';
+import CustomButton from '../../../components/CustomButton';
+
+const LoginPage = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.logo}>
+        <AppLogo />
+        <Text style={styles.logoText}>로그인</Text>
+      </View>
+      <View style={styles.formArea}>
+        <MyTextInput placeholder="아이디" />
+        <MyTextInput placeholder="비밀번호" />
+      </View>
+      <View style={styles.textArea}>
+        <Text>App이름 회원이 아니신가요?</Text>
+        <TouchableOpacity>
+          <Text>회원가입 하기</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity onPress={() => console.log('버튼 눌림')}>
+        <CustomButton title="로그인" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FCFDE1',
+    height: '100%',
+    padding: 25,
+  },
+  logo: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  logoText: {
+    fontSize: 26,
+    marginTop: 30,
+    marginBottom: 20,
+    color: '#35562F',
+    fontWeight: 'bold',
+  },
+  formArea: {
+    marginTop: 40,
+  },
+  textArea: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 130,
+  },
+});
+
+export default LoginPage;
