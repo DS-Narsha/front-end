d4617fa7c74f1c93cbeb3dc6cced4d49e4a37ab2
yxxyn20 <dbtndus0527@gmail.com>
2023-06-06 20:44:06 +0900

    [feat] 시작 화면 구현 - 시작 화면 구현 - navigation을 사용하여 로그인, 회원가입 페이지 이동 구현

---
+++ narsha_front/src/pages/StartPage.tsx
@@ -0,0 +1,73 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
+import CustomButton from '../components/CustomButton.tsx';
+
+//@ts-ignore
+const StartPage = ({navigation}) => {
+  return (
+    <ImageBackground
+      source={require('../assets/start-bg.jpg')}
+      style={styles.container}
+      imageStyle={{opacity: 0.8}}>
+      <View style={styles.blank} />
+      <View style={styles.introContainer}>
+        <Text style={styles.introTitle}>App Name</Text>
+        <Text style={styles.introContent}>
+          안녕하세요! SNS 교육 플랫폼 AppName입니다! 설명설명~
+        </Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <CustomButton
+          title={'로그인'}
+          color={'#E3F1A9'}
+          navi={navigation}
+          stack="Login"
+        />
+        {/* height */}
+        <View style={{height: 15}} />
+        <CustomButton
+          title={'회원가입'}
+          color={'#AADF98'}
+          navi={navigation}
+          stack="UserType"
+        />
+      </View>
+      {/* height */}
+      <View style={{height: 130}} />
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    backgroundColor: 'white',
+    paddingHorizontal: 25,
+    flexDirection: 'column',
+  },
+  introContainer: {
+    flex: 2,
+  },
+  introTitle: {
+    fontWeight: '600',
+    color: '#61A257',
+    fontSize: 40,
+  },
+  introContent: {
+    width: 220,
+    fontSize: 16,
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+  blank: {
+    flex: 1,
+  },
+});
+
+export default StartPage;
