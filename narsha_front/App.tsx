fb6a2a6ac2205d1b4cc314d994ec8ae7625a6a0a
yxxyn20 <dbtndus0527@gmail.com>
2023-06-06 17:52:56 +0900

    [feat] 스플래시 화면 생성 및 실행 - react-native-splash-screen 설치 - SplashActivity.java 연결

--- narsha_front/App.tsx
+++ narsha_front/App.tsx
@@ -1,15 +1,16 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import MainNavigator from './src/components/navigation/MainNavigator';
-import LoginPage from './src/pages/Login/LoginPage';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
-    
     <NavigationContainer>
       <MainNavigator />
     </NavigationContainer>
-
   );
 }
