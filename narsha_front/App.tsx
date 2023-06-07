2aad127374069c9ca37426f1955671c180a93a1c
yxxyn20 <dbtndus0527@gmail.com>
2023-06-07 18:19:15 +0900

    [feat] KeyboardAvoidingView로 키보드 가림 설정 - KeyboardAvoidingView로 키보드가 textInput 가림 문제 수정

--- narsha_front/App.tsx
+++ narsha_front/App.tsx
@@ -3,6 +3,7 @@ import React, {useEffect} from 'react';
 import MainNavigator from './src/components/navigation/MainNavigator';
 import SplashScreen from 'react-native-splash-screen';
 import AuthStack from './src/components/navigation/AuthStack';
+import {KeyboardAvoidingView, StyleSheet, Platform, View} from 'react-native';
 
 const isLoggedIn = false;
 
@@ -12,8 +13,24 @@ export default function App() {
   }, []);
 
   return (
-    <NavigationContainer>
-      {isLoggedIn ? <MainNavigator /> : <AuthStack />}
-    </NavigationContainer>
+    <View style={styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.select({ios: 'padding', android: undefined})}
+        style={styles.avoid}>
+        <NavigationContainer>
+          {isLoggedIn ? <MainNavigator /> : <AuthStack />}
+        </NavigationContainer>
+      </KeyboardAvoidingView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  avoid: {
+    flex: 1,
+  },
+});
