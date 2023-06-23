ffd13c3aa5dbb042aefe48c2ab1ed60d505becab
otcroz <dbtndus0527@gmail.com>
2023-06-23 17:53:01 +0900

    [style] 게시글 작성 페이지 style - 게시글 작성 페이지 코드 작성 - 추후에 마스킹 적용한 사진 목록 불러오기, 상태 관리 필요

--- narsha_front/src/pages/post/WritePage.tsx
+++ narsha_front/src/pages/post/WritePage.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {StyleSheet, View, Image, Text} from 'react-native';
 import ArrowLeft from '../../assets/arrow-left.svg';
 import SendBtn from '../../assets/send-btn.svg'
+import NextPhoto from '../../assets/next-photo.svg'
+import PrevPhoto from '../../assets/prev-photo.svg'
 
 const styles = StyleSheet.create({
   container:{
@@ -35,6 +37,66 @@ dot:{
   borderRadius: 50,
   margin: 20,
 },
+pickImg: {
+  height: 280,
+  width: 280,
+  borderRadius: 10,
+  paddingHorizontal: 16,
+  backgroundColor: '#C0C0C0',
+  marginHorizontal: 10
+},
+contentContainer:{
+  flex: 1,
+},
+uploadContentBox:{
+  flex: 1,
+  backgroundColor: '#ffffff',
+  borderRadius: 20,
+  shadowOffset: {
+    width: 5,
+    height: -10,
+  },
+  elevation: 10,
+},
+writingBox:{
+  display: 'flex',
+  flexDirection: 'row',
+  paddingHorizontal: 18,
+  marginTop: 25,
+  marginBottom: 10
+},
+profile: {
+  width: 45,
+  height: 45,
+  borderRadius: 50,
+  backgroundColor: '#C0C0C0',
+  marginRight: 13
+},
+content:{
+  flex: 1,
+  height: 90,
+  borderRadius: 10,
+  backgroundColor: '#C0C0C0'
+},
+gridView: {
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  paddingTop: 7,
+  justifyContent: 'center',
+},
+img: {
+  backgroundColor: '#C0C0C0',
+  borderRadius: 10,
+  width: 55,
+  height: 55,
+  marginHorizontal: 5
+},
+selectPhotoBox:{
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center'
+}
 });
 
 //@ts-ignore
@@ -47,14 +109,44 @@ const WritePage = ({navigation}) => {
           <ArrowLeft/>
           <View style={styles.progress}>
             <View style={[styles.dot, {backgroundColor: '#98DC63'}]}/>
-            <View style={[styles.dot, {backgroundColor: '#D9D9D9'}]}/>
-            <View style={[styles.dot, {backgroundColor: '#D9D9D9'}]}/>
+            <View style={[styles.dot, {backgroundColor: '#98DC63'}]}/>
+            <View style={[styles.dot, {backgroundColor: '#98DC63'}]}/>
           </View>
           <SendBtn />
         </View>
         <Text>글을 작성해볼까요?</Text>
       </View>
       {/* content */}
+      <View style={styles.contentContainer}>
+        <View style={styles.selectPhotoBox}>
+          <PrevPhoto />
+          <View style={{alignItems: 'center', marginTop: 20}}>
+            <View style={styles.pickImg} />
+          </View>
+          <NextPhoto />
+        </View>
+        {/* writing*/}
+        <View style={styles.uploadContentBox}>
+          <View style={styles.writingBox}>
+            <View style={styles.profile}></View>
+            <View style={styles.content}></View>
+          </View>
+          <View style={styles.gridView}>
+            <View style={styles.img}></View>
+            <View style={styles.img}></View>
+            <View style={styles.img}></View>
+            <View style={styles.img}></View>
+            <View style={styles.img}></View>
+          </View>
+          <View style={styles.gridView}>
+            <View style={styles.img}></View>
+            <View style={styles.img}></View>
+            <View style={styles.img}></View>
+            <View style={styles.img}></View>
+            <View style={styles.img}></View>
+          </View>
+        </View>
+      </View>
       <Text>WritePage</Text>
     </View>
   );
