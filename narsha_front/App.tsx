5ada6b4819fddaee771fd371021644adfde6f2c9
pyounani <yndbsk9372@gmail.com>
2023-09-04 20:49:19 +0900

    [feat] firebase 기능 구현

--- narsha_front/App.tsx
+++ narsha_front/App.tsx
@@ -1,14 +1,15 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React, {useEffect, useState, createContext} from 'react';
+import React, {useEffect, useState, createContext, useCallback} from 'react';
 import MainNavigator from './src/components/navigation/MainNavigator';
 import SplashScreen from 'react-native-splash-screen';
 import AuthStack from './src/components/navigation/AuthStack';
 import NotAvailable from './src/pages/NotAvailablePage';
-import {KeyboardAvoidingView, StyleSheet, Platform, View} from 'react-native';
+import {KeyboardAvoidingView, StyleSheet, Platform, View, Alert, Text, TouchableOpacity, Modal} from 'react-native';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {StartTimeContext} from './src/components/StartTimeContext';
 import {EndTimeContext} from './src/components/EndTimeContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import messaging, { FirebaseMessagingTypes } from '@react-native-firebase/messaging';
 
 const isLoggedIn = false;
 // const [possible, setPossible] = useState(true);
@@ -28,8 +29,25 @@ export const getData = async (key: string) => {
     console.log(e.message);
   }
 };
-
 export default function App() {
+  const [notification, setNotification] = useState<any>(null);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const foregroundListener = useCallback(() => {
+    messaging().onMessage(async message => {
+      if (message.notification) {
+        // 모달 표시
+        setModalVisible(true);
+        setNotification(message.notification);
+        console.log(message);
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    foregroundListener()
+  }, [])
+
   const [startTime, setStartTime] = useState(new Date());
   const [endTime, setEndTime] = useState(new Date());
 
@@ -81,6 +99,30 @@ export default function App() {
       <EndTimeContext.Provider
         value={{endTime: endTime, setEndTime: setEndTime}}>
         <View style={styles.container}>
+
+        <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+        >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>푸시 알림 내용:</Text>
+            <Text>{notification ? notification.body : ''}</Text>
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text>닫기</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        </Modal>
+
+      
           <QueryClientProvider client={queryClient}>
             <KeyboardAvoidingView
               behavior={Platform.select({ios: 'padding', android: undefined})}
@@ -95,6 +137,7 @@ export default function App() {
         </View>
       </EndTimeContext.Provider>
     </StartTimeContext.Provider>
+
   );
 }
 
@@ -105,5 +148,22 @@ const styles = StyleSheet.create({
   },
   avoid: {
     flex: 1,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+    marginBottom: 40
+  },
+  closeButton: {
+    marginTop: 10,
+    alignSelf: 'flex-end',
   },
 });
