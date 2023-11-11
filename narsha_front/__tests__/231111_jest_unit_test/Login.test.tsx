8d7b721f611dfb72dbdbe5f87409c9d1c98e5126
otcroz <dbtndus0527@gmail.com>
2023-11-12 01:17:10 +0900

    [test] user login test: didn't enter ID - user login test: didn't enter ID

---
+++ narsha_front/__tests__/231111_jest_unit_test/Login.test.tsx
@@ -0,0 +1,33 @@
+import {fireEvent, render, screen} from '@testing-library/react-native';
+import {userEvent} from '@testing-library/user-event';
+import TestLoginPage from '../../test/TestLoginPage';
+import '@testing-library/jest-dom';
+import alertMock, {
+  setAlertMock,
+  restoreAlertMock,
+} from '../../__mocks__/alertMock';
+
+describe('LoginPage', () => {
+  beforeEach(() => {
+    setAlertMock();
+  });
+
+  afterEach(() => {
+    restoreAlertMock();
+  });
+
+  it('아이디를 입력하지 않았을 때', async () => {
+    // 1. Arrange
+    render(<TestLoginPage />);
+
+    // 2. Act
+    const inputID = screen.getByPlaceholderText('아이디');
+    const TouchableOpacity = screen.getByText('확인!');
+
+    // 3. Assert
+    fireEvent.changeText(inputID, '');
+    fireEvent.press(TouchableOpacity);
+
+    expect(alertMock).toHaveBeenCalledWith('아이디를 입력해주세요.');
+  });
+});
